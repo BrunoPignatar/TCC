d7ebf23b914e3f511a83933ca7bbb4a32b5cebe2
Usuario <Usuario@CPD03-M15>
2023-04-20 10:33:54 -0300

    ajustes na interface e criação dos botoes da seleção de mundo

--- autplay/autplay/View/selecao.xaml.cs
+++ autplay/autplay/View/selecao.xaml.cs
@@ -17,5 +17,44 @@ namespace autplay.View
             InitializeComponent();
             logo.Source = ImageSource.FromResource("autplay.Assets.logo.png");
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new autplay.selectword.Mundo1());
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new autplay.selectword.Mundo2());
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new autplay.selectword.Mundo3());
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
     }
 }
