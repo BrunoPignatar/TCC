b793f4189394164f47d596565b2a6d47556656e5
Usuario <Usuario@CPD03-M15>
2023-05-08 15:52:10 -0300

    fazendo carousel e adicionando os avatares

---
+++ autplay/autplay/Model/Avatares.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace autplay.Model
+{
+    public class Avatares
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Imagem { get; set;}
+    }
+}
--- autplay/autplay/View/Menu.xaml.cs
+++ autplay/autplay/View/Menu.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using autplay.Model;
 using autplay.View;
 
 using Xamarin.Forms;
@@ -13,6 +15,7 @@ namespace autplay.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Menu : ContentPage
     {
+        ObservableCollection<Avatares> list;
         public Menu()
         {
             InitializeComponent();
@@ -21,27 +24,40 @@ namespace autplay.View
             btn_add.Source = ImageSource.FromResource("autplay.Assets.botaoadd.png");
             //REMOVENDO A NAVBAR
             NavigationPage.SetHasNavigationBar(this, false);
-        }
 
-        //BOTÃO QUE LEVA A TELA DE SELEÇÃO-- OK
-        private async void btn_jogar_Clicked(object sender, EventArgs e)
-        {
-            try
+            //ADICIONANDO OS AVATARES
+            list = new ObservableCollection<Avatares>
             {
-                await Navigation.PushAsync(new autplay.View.selecao());
+                new Avatares{Nome="juan",Imagem="https://tinyurl.com/sm59xs32"},
+                new Avatares{Nome="Cachorro",Imagem="https://tinyurl.com/4fujmffr"},
+                new Avatares{Nome="Gato",Imagem="https://tinyurl.com/2zbbx3an"},
+                new Avatares{Nome="Peixe",Imagem="https://tinyurl.com/445abh2e"},
+                new Avatares{Nome="Arara",Imagem="https://tinyurl.com/3n4dz6nu"},
+        };
+            avatares.ItemsSource = list;
 
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("Error", ex.Message, "OK");
-            }
-        }
-
-        //BOTÃO DE ADICIONAR FOTO COMO AVATAR--
-        private void btn_add_Clicked(object sender, EventArgs e)
-        {
-
-        }
 
     }
+
+    //BOTÃO QUE LEVA A TELA DE SELEÇÃO-- OK
+    private async void btn_jogar_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            await Navigation.PushAsync(new autplay.View.selecao());
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
+    //BOTÃO DE ADICIONAR FOTO COMO AVATAR--
+    private void btn_add_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+}
 }
