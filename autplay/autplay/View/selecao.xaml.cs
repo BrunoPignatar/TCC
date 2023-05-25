caa35b6e41d31c3043579398833438c588477535
Usuario <Usuario@CPD02-M18>
2023-05-25 15:10:55 -0300

    começando a pagina seleçao de mundos

--- autplay/autplay/View/selecao.xaml.cs
+++ autplay/autplay/View/selecao.xaml.cs
@@ -18,7 +18,11 @@ namespace autplay.View
             //REMOVENDO A NAVBAR
             NavigationPage.SetHasNavigationBar(this, false);
 
-            
+            btn_wordapocalypse.Source = ImageSource.FromResource("autplay.Assets.mundo2.png");
+            btn_wordgum.Source = ImageSource.FromResource("autplay.Assets.mundo3.png");
+            btn_wordrose.Source = ImageSource.FromResource("autplay.Assets.mundo1.png");
+
+
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
@@ -47,6 +51,21 @@ namespace autplay.View
             {
                 DisplayAlert("Error", ex.Message, "OK");
             }
+        }
+
+        private void btn_wordrose_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_wordapocalypse_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_wordgum_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
